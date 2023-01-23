834b0bd8831d180c6a3ac062087c142260c49c87
Matthieu Monsch <1216372+mtth@users.noreply.github.com>
2023-01-23 09:56:48 -0800

    Add info method (#11)

---
+++ packages/highs-solver/test/solver.test.ts
@@ -0,0 +1,13 @@
+import * as sut from '../src/solver';
+
+describe('solver', () => {
+  test('returns unset status', () => {
+    const solver = sut.Solver.create();
+    expect(solver.getStatus()).toEqual(sut.SolverStatus.NOT_SET);
+  });
+
+  test('returns unset info', () => {
+    const solver = sut.Solver.create();
+    expect(solver.getInfo()).toMatchObject({basisIsValid: false});
+  });
+});
