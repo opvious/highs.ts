31f46491cf57955fd426e2e268f9f094fc3f1226
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-03-18 10:54:45 -0700

    Change model and solution in-memory representations

--- packages/highs-solver/README.md
+++ packages/highs-solver/README.md
@@ -38,29 +38,6 @@ const solution = await highs.solve('my-model.lp', {
 All file formats (`.lp`, `.mps`, ...) and [options][highs-options] supported by
 HiGHS may be passed in.
 
-### Solve inline model
-
-```typescript
-const solution = await highs.solve({
-  objective: {
-    isMaximization: true,
-    weights: highs.sparseRow([1, 2, 4, 1]),
-    offset: 10,
-  },
-  variables: [
-    {type: highs.VariableType.CONTINUOUS, lowerBound: 0, upperBound: 40},
-    {type: highs.VariableType.CONTINUOUS},
-    {type: highs.VariableType.CONTINUOUS},
-    {type: highs.VariableType.CONTINUOUS, lowerBound: 2, upperBound: 3},
-  ],
-  constraints: [
-    {weights: highs.sparseRow([-1, 1, 1, 10]), upperBound: 20},
-    {weights: highs.sparseRow([1, -4, 1]), upperBound: 30},
-    {weights: highs.sparseRow([1, -0.5], [1, 3]), lowerBound: 0, upperBound: 0},
-  ],
-});
-```
-
 ### Monitor solving progress
 
 ```typescript
--- packages/highs-solver/src/solver.ts
+++ packages/highs-solver/src/solver.ts
@@ -1,22 +1,11 @@
 import {assert, errorFactories} from '@opvious/stl-errors';
 import {noopTelemetry, Telemetry} from '@opvious/stl-telemetry';
-import {ifPresent} from '@opvious/stl-utils';
 import {writeFile} from 'fs/promises';
 import * as addon from 'highs-addon';
 import * as tmp from 'tmp-promise';
 import util from 'util';
 
-import {
-  assertBalanced,
-  Constraint,
-  Model,
-  packageInfo,
-  Solution,
-  SolutionStyle,
-  SparseRow,
-  sparseRow,
-  Variable,
-} from './common';
+import {packageInfo, SolutionStyle} from './common';
 import {SolveMonitor, SolveTracker} from './monitor';
 
 const [errors, codes] = errorFactories({
@@ -68,26 +57,22 @@ export class Solver {
   }
 
   /** Sets the model to be solved. */
-  setModel(model: Model): void {
+  setModel(model: SolverModel): void {
     this.assertNotSolving();
     this.telemetry.logger.debug('Setting inline model.');
-    const {objective: obj, constraints, variables} = model;
-    const width = variables.length;
-    this.delegate.passModel({
-      columnCount: width,
-      rowCount: constraints.length,
-      isMaximization: obj?.isMaximization ?? false,
-      offset: obj?.offset ?? 0,
-      costs:
-        ifPresent(obj?.weights, (w) => densifyRow(w, width)) ??
-        new Float64Array(width),
-      matrix: computeMatrix(constraints),
-      columnLowerBounds: densify(variables, (v) => v.lowerBound ?? -Infinity),
-      columnUpperBounds: densify(variables, (v) => v.upperBound ?? Infinity),
-      rowLowerBounds: densify(constraints, (v) => v.lowerBound ?? -Infinity),
-      rowUpperBounds: densify(constraints, (v) => v.upperBound ?? Infinity),
-      integrality: computeIntegrality(variables),
-    });
+    const width = model.columnTypes.length;
+    const height = model.rowLowerBounds.length;
+    assert(
+      model.columnLowerBounds.length === width &&
+        model.columnUpperBounds.length === width,
+      'Inconsistent width'
+    );
+    assert(
+      model.rowUpperBounds.length === height &&
+        model.weights.offsets.length <= height,
+      'Inconsistent height'
+    );
+    this.delegate.passModel({columnCount: width, rowCount: height, ...model});
   }
 
   /**
@@ -154,7 +139,7 @@ export class Solver {
       await tel.withActiveSpan({name: 'HiGHS solve'}, async (span) => {
         await this.promisified('run');
         status = this.getStatus();
-        span.setAttribute('solve.status', SolverStatus[status]);
+        span.setAttribute('solver.status', SolverStatus[status]);
       });
     } catch (cause) {
       throw errors.solveFailed(this, cause);
@@ -189,27 +174,21 @@ export class Solver {
   }
 
   /** Returns the current solution, set from the last solve. */
-  getSolution(): Solution | undefined {
+  getSolution(): SolverSolution | undefined {
     const sol = this.delegate.getSolution();
     if (!sol.isValueValid) {
       return undefined;
     }
     return {
-      primal: {
-        variables: sparseRow(sol.columnValues),
-        constraints: sparseRow(sol.rowValues),
-      },
+      primal: {rows: sol.rowValues, columns: sol.columnValues},
       dual: sol.isDualValid
-        ? {
-            variables: sparseRow(sol.columnDualValues),
-            constraints: sparseRow(sol.rowDualValues),
-          }
+        ? {rows: sol.rowDualValues, columns: sol.columnDualValues}
         : undefined,
     };
   }
 
   /** Write the current solution to the given path. */
-  async writeSolution(fp: string, style?: SolutionStyle): Promise<void> {
+  async writeSolution(fp: string, style?: addon.SolutionStyle): Promise<void> {
     this.assertNotSolving();
     const {telemetry: tel} = this;
     tel.logger.debug('Writing solution to %j...', fp);
@@ -250,54 +229,16 @@ export interface SolverCreationOptions {
 
 export type SolverInfo = addon.Info;
 
-function densifyRow(row: SparseRow, size: number): Float64Array {
-  const arr = new Float64Array(size);
-  const {indices, values} = row;
-  assertBalanced(row);
-  for (const [ix, vix] of indices.entries()) {
-    arr[vix] = values[ix]!;
-  }
-  return arr;
+export type SolverModel = Omit<addon.Model, 'columnCount' | 'rowCount'>;
+
+export interface SolverSolution {
+  readonly primal: SolverSolutionValues;
+  readonly dual?: SolverSolutionValues;
 }
 
-function computeMatrix(
-  constraints: ReadonlyArray<Constraint>
-): addon.SparseMatrix {
-  let weightCount = 0;
-  for (const {weights} of constraints) {
-    assertBalanced(weights);
-    weightCount += weights.indices.length;
-  }
-  const starts = new Int32Array(constraints.length);
-  const indices = new Int32Array(weightCount);
-  const values = new Float64Array(weightCount);
-  let wix = 0;
-  for (const [rix, {weights: w}] of constraints.entries()) {
-    starts[rix] = wix;
-    indices.set(w.indices, wix);
-    values.set(w.values, wix);
-    wix += w.indices.length;
-  }
-  return {isColumnOriented: false, starts, indices, values};
-}
-
-function densify<V>(
-  gen: ReadonlyArray<V>,
-  fn: (val: V) => number
-): Float64Array {
-  const arr = new Float64Array(gen.length);
-  for (const [ix, val] of gen.entries()) {
-    arr[ix] = fn(val);
-  }
-  return arr;
-}
-
-function computeIntegrality(vars: ReadonlyArray<Variable>): Int32Array {
-  const arr = new Int32Array(vars.length);
-  for (const [ix, v] of vars.entries()) {
-    arr[ix] = +v.type;
-  }
-  return arr;
+export interface SolverSolutionValues {
+  readonly rows: Float64Array;
+  readonly columns: Float64Array;
 }
 
 export interface SolverOptions extends addon.CommonOptions {
